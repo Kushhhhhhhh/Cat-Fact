bf72772056b862b8fced733d710453047913a7e2
kush sharma <kush.73888@gmail.com>
2024-02-07 16:42:48 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <main>
+      <div>
+        <h1>Cat Facts</h1>
+        <img src="" alt="" />
+      </div>
+
+      <div>
+        <button>Get Fact</button>
+      </div>
+    </main>
+  )
+}
+
+export default App
